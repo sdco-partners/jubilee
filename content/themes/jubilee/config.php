907eacf7bd1cb248404328d082f109c4bb800279
Asifuzzaman Ahmed <aa@hellofriends.org>
2017-08-15 14:00:15 -0400

    remove data

--- content/themes/jubilee/config.php
+++ content/themes/jubilee/config.php
@@ -12,10 +12,5 @@
  * @version 1.0
  */
 
-  $account_id = "1354228"; 
-  $public_api_key = "379bd93645bae5709eed"; 
-  $private_api_key = "a8cb671869bf4936b281"; 
-  $groups = array(7035380); 
-
 
 ?>
